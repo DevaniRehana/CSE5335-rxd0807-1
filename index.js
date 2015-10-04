01303fbf43320fdd4d6e7f41943d6e24f5af8ddb
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-10-04 00:45:47 -0500

    code push

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ app.get('/', function(request, response) {
 
 app.get('/json', function(request, response) {
 var list1 = [
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"Anna", "lastName":"Smith"},
-    {"firstName":"Peter", "lastName":"Jones"},
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"John", "lastName":"Doe"}, 
-    {"firstName":"John", "lastName":"Doe"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"-34.397,150.644"}, 
+    {"firstName":"Anna", "lastName":"Smith", "location":"-30.397,15.644"},
+    {"firstName":"Peter", "lastName":"Jones", "location":"-20.397,-50.644"},
+    {"firstName":"John", "lastName":"Doe", "location":"-99.397,50.644"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"-84.397,-10.644"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"10.397,25.644"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"92.397,82.644"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"118.397,67.644"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"-118.397,76.644"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"84.397,-65.644"}, 
 ]
 
   response.json(list1);
