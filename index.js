f0c89a0c26d54af99aca5092e40830826263dbc7
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-10-06 18:08:00 -0500

    code push

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ app.get('/', function(request, response) {
 
 app.get('/json', function(request, response) {
 var list1 = [
-    {"firstName":"John", "lastName":"Doe", "location":"-33.890542,151.274856"}, 
-    {"firstName":"Anna", "lastName":"Smith", "location":"-33.923036,151.259052"},
-    {"firstName":"Peter", "lastName":"Jones", "location":"-34.028249,151.157507"},
-    {"firstName":"John", "lastName":"Doe", "location":"-33.80010128657071,151.28747820854187"}, 
-    {"firstName":"Stephen", "lastName":"Morgan", "location":"-33.950198,151.259302"}, 
-    {"firstName":"Danny", "lastName":"James", "location":"-32.9567,151.954"}, 
-    {"firstName":"Mike", "lastName":"James", "location":"-32.80010,151.1575"}, 
-    {"firstName":"Rose", "lastName":"Bush", "location":"-32.0386,151.9576"}, 
-    {"firstName":"Jenny", "lastName":"Clinton", "location":"-33. 2867,151.1516"}, 
-    {"firstName":"Alex", "lastName":"Trump", "location":"-33.9876,151.27468"}, 
+    {"firstName":"John", "lastName":"Doe", "location":"-33.890542,151.274856", "value":8}, 
+    {"firstName":"Anna", "lastName":"Smith", "location":"-33.923036,151.259052","value":12},
+    {"firstName":"Peter", "lastName":"Jones", "location":"-34.028249,151.157507","value":1},
+    {"firstName":"Obama", "lastName":"Barack", "location":"-33.80010128657071,151.28747820854187","value":15}, 
+    {"firstName":"Stephen", "lastName":"Morgan", "location":"-33.950198,151.259302","value":9}, 
+    {"firstName":"Danny", "lastName":"James", "location":"-32.9567,151.954","value":50}, 
+    {"firstName":"Mike", "lastName":"James", "location":"-32.80010,151.1575","value":5}, 
+    {"firstName":"Jeb", "lastName":"Bush", "location":"-32.0386,151.9576","value":29}, 
+    {"firstName":"Jenny", "lastName":"Clinton", "location":"-33. 2867,151.1516","value":22}, 
+    {"firstName":"Donald", "lastName":"Trump", "location":"-33.9876,151.27468","value":2}, 
 ]
 
   response.json(list1);
